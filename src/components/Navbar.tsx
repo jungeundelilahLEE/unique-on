37629b1dab9ce1e108fa8a1ea5c7d17f7f0837d8
jungeundelilahLEE <delilah@seoullabs.io>
2025-04-02 11:46:14 +0900

    feat: 모바일 메뉴 오버레이 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -96,6 +96,21 @@ const MobileNavLink = styled(Link)`
   }
 `;
 
+const MobileMenuOverlay = styled.div<MobileMenuProps>`
+  display: ${({ isOpen }) => (isOpen ? "block" : "none")};
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 30;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
+
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -133,6 +148,7 @@ export default function Navbar() {
           </svg>
         </MobileMenuButton>
       </Container>
+      <MobileMenuOverlay isOpen={isMenuOpen} onClick={toggleMenu} />
       <MobileMenu isOpen={isMenuOpen}>
         <MobileNavLink href="/about" onClick={() => setIsMenuOpen(false)}>
           About
